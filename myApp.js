d40b578535732ffd01b1042026f9ce0bb4500cb5
gerimelanic <ndungugerishon19@gmail.com>
2025-06-18 21:15:46 -0500

    FIx double listening error

--- myApp.js
+++ myApp.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 const helmet = require ('helmet');
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => {
-  console.log(`🦾Geri Melanic Info Security App Started on Port ${PORT}`);
-});
 
 
 
@@ -63,7 +59,6 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`🦾Geri Melanic Info Security App Started on Port ${PORT}`);
 });
